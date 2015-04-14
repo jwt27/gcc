aca02ab22bb329fab7b4f0ea687a1c6949f07564
Andris Pavenis <andris.pavenis@iki.fi>
2015-04-14 20:09:23 +0300

    [DJGPP] djbuild/djconfig.sh: do not build native Ada compiler by default

--- djgpp/djbuild/djconfig.sh
+++ djgpp/djbuild/djconfig.sh
@@ -20,8 +20,8 @@ export CONFIG_SHELL
 conf_options="$target --prefix=/dev/env/DJDIR"
 #conf_options="$target --disable-nls"
 conf_options="$target --disable-werror"
-conf_options="$conf_options --enable-languages=c,c++,fortran,objc,obj-c++,ada"
-#conf_options="$conf_options --enable-languages=c,c++,fortran,objc,obj-c++"
+#conf_options="$conf_options --enable-languages=c,c++,fortran,objc,obj-c++,ada"
+conf_options="$conf_options --enable-languages=c,c++,fortran,objc,obj-c++"
 conf_options="$conf_options --enable-libquadmath-support"
 conf_options="$conf_options --enable-lto"
 #
