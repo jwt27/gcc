f89b7a994fd508d7ac47c8fce013b694e033b70f
Andris Pavenis <andris.pavenis@iki.fi>
2015-12-26 09:15:25 +0200

    [DJGPP] Updates to DJGPP build system

--- djgpp/cross-native/config-cross-native.sh
+++ djgpp/cross-native/config-cross-native.sh
@@ -5,20 +5,28 @@
 #
 # Configuring gcc for cross-native build og DJGPP port under Linux
 # 
-# - only C and C++ compilers are included now
 # - one must install GMP, MPFR, MPC and ZLIB development files
 #   for DJGPP
 #
 
+export PATH=$(pwd)/../tmpinst/bin:$PATH
+export LD_LIBRARY_PATH=$(pwd)/../tmpinst/lib64
+
+gcc -v 
+i586-pc-msdosdjgpp-gcc -v
+
+
 target=i586-pc-msdosdjgpp
 
-../gnu/gcc-4.80-20120906/configure \
+../gnu/gcc-6.00-20151211/configure \
+    --prefix=/dev/env/DJDIR \
     --build=i586-pc-linux-gnu \
     --host=i586-pc-msdosdjgpp \
     --target=$target \
-    --enable-languages="c,c++" \
+    --enable-languages="c,c++,fortran,objc,obj-c++," \
     --with-native-system-header-dir=/usr/$target/sys-include \
     --disable-libstdcxx-pch \
-    --disable-nls
+    --enable-lto \
+    --enable-nls || exit 1
 
-
+make -j12 | exit 1
