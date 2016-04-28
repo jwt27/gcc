8803fdbf1a3121c46441eb63605b2541c4c1b79a
Andris Pavenis <andris.pavenis@iki.fi>
2016-04-28 21:33:31 +0300

    [DJGPP] gcc/ginclude/stdint-wrap.h: cleanup old DJGPP changes

--- gcc/ginclude/stdint-wrap.h
+++ gcc/ginclude/stdint-wrap.h
@@ -6,16 +6,7 @@
 #  undef __STDC_CONSTANT_MACROS
 #  define __STDC_CONSTANT_MACROS
 # endif
-# if defined(__DJGPP__)
-#  include <sys/version.h>
-#  if __DJGPP__<2 || (__DJGPP__==2 && __DJGPP_MINOR__<=3)
-#   include "stdint-gcc.h"
-#  else
-#   include_next <stdint.h>
-#  endif   	
-# else
-#  include_next <stdint.h>
-# endif
+# include_next <stdint.h>
 #else
 # include "stdint-gcc.h"
 #endif
