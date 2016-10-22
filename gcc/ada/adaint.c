af177075f4f24862ba8f21c2c673ab1f96843128
Andris Pavenis <andris.pavenis@iki.fi>
2016-10-22 08:26:42 +0300

    [DJGPP] adaint.c (__gnat_get_current_dir) convert '/' to '\\' for DJGPP

--- gcc/ada/adaint.c
+++ gcc/ada/adaint.c
@@ -615,7 +615,9 @@ __gnat_get_default_identifier_character_set (void)
 void
 __gnat_get_current_dir (char *dir, int *length)
 {
-#if defined (__MINGW32__)
+#ifdef __DJGPP__
+  int i;
+#elif defined (__MINGW32__)
   TCHAR wdir[GNAT_MAX_PATH_LEN];
 
   _tgetcwd (wdir, *length);
@@ -627,6 +629,14 @@ __gnat_get_current_dir (char *dir, int *length)
 #endif
 
    *length = strlen (dir);
+
+#if __DJGPP__
+   for (i = 0; i < *length; i++)
+     {
+       if (dir[i] == '/')
+	 dir[i] = '\\';
+     }
+#endif
 
    if (dir [*length - 1] != DIR_SEPARATOR)
      {
