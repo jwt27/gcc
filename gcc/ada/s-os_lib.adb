12a08461811e5697e779cc275f106e83418f6619
Andris Pavenis <andris.pavenis@iki.fi>
2017-01-22 19:30:15 +0200

    [DJGPP] adaint.c, s_os_lib.adb: implement change rejected from upstream in a different way

--- gcc/ada/s-os_lib.adb
+++ gcc/ada/s-os_lib.adb
@@ -2246,11 +2246,8 @@ package body System.OS_Lib is
       end File_Name_Conversion;
 
       --  Replace all '/' by Directory Separators (this is for Windows)
-      --  No need to do that however for DJGPP
 
-      if Directory_Separator /= '/'
-        and then Is_Djgpp = 0
-      then
+      if Directory_Separator /= '/' then
          for Index in 1 .. End_Path loop
             if Path_Buffer (Index) = '/' then
                Path_Buffer (Index) := Directory_Separator;
