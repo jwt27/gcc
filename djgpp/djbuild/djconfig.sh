2cc05c53c74a08157494e9322b4f48c6170ba386
Andris Pavenis <andris@localhost.localdomain>
2017-06-26 12:14:49 +0300

    [DJGPP] Enable std::experimental::filesystem (not yet ready)
    # Conflicts:
    #	djgpp/djbuild/djconfig.sh

--- djgpp/djbuild/djconfig.sh
+++ djgpp/djbuild/djconfig.sh
@@ -18,12 +18,13 @@ case $CONFIG_SHELL in *.exe) ;; *) CONFIG_SHELL=$CONFIG_SHELL.exe ;; esac
 export CONFIG_SHELL
 #
 conf_options="$target --prefix=/dev/env/DJDIR"
-#conf_options="$target --disable-nls"
 conf_options="$target --disable-werror"
-#conf_options="$conf_options --enable-languages=c,c++,fortran,objc,obj-c++,ada"
-conf_options="$conf_options --enable-languages=c,c++,fortran,objc,obj-c++"
+conf_options="$conf_options --enable-languages=c,c++,fortran,objc,obj-c++,ada"
 conf_options="$conf_options --enable-libquadmath-support"
 conf_options="$conf_options --enable-lto"
+conf_options="$conf_options --disable-libstdcxx-pch"
+conf_options="$conf_options --enable-libstdcxx-filesystem-ts"
+#conf_options="$target --disable-nls"
 #
 srcdir=`(cd $src && pwd) | sed -e 's,^[a-zA-Z]:/,/,' -e 's,^/dev/[a-zA-Z]/,/,'`
 builddir=`pwd | sed -e 's,^[a-zA-Z]:/,/,' -e 's,^/dev/[a-zA-Z]/,/,'`
